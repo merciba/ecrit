4fc6b775408b3438172db45602ac1ed1a6e26aa8
merciba <safeuser@merciba.com>
2015-03-27 12:33:28 -0400

    Separation of command concerns

--- bin/ecrit.js
+++ bin/ecrit.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
-require('coffee-script').register();
-require('./entry.litcoffee')
+require('coffee-script').register()
+fs = require('fs-sync')
+
+done = function (exitmsg) {
+	if (exitmsg) console.log(exitmsg)
+	process.exit(0)
+}
+
+if (fs.exists(__dirname+"/"+process.argv[2]+".litcoffee")) require("./"+process.argv[2]+".litcoffee")(done) 
+else console.log("Not a valid parameter. \n\n\t`ecrit [create|test] [args...]`")
 
